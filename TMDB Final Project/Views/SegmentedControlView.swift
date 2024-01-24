b877b64917c8603552efa94321ae14714db78b03
Danylo <semenenkodaniel99@gmail.com>
2024-01-24 11:56:25 -0800

    Added and customized segmentedControl + randomButton.

---
+++ TMDB Final Project/Views/SegmentedControlView.swift
@@ -0,0 +1,20 @@
+//
+//  SegmentedControlView.swift
+//  TMDB Final Project
+//
+//  Created by xcode on 2024-01-23.
+//
+
+import UIKit
+
+class SegmentedControlView: UISegmentedControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
